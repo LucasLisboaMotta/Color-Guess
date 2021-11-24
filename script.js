f9e6ea6d03428bc2a1ee597db42ac194965f0431
Lucs Lisboa <lucas@pop-os.localdomain>
2021-11-24 16:40:59 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let circuloCerto
+const circulosColoridos = document.getElementsByClassName('ball');
+const FraseAcerto = document.getElementById('answer');
+
 function gerarCores() {
     const cor1 = Math.floor(Math.random() * 255);
     const cor2 = Math.floor(Math.random() * 255);
@@ -5,11 +9,10 @@ function gerarCores() {
     return '(' + cor1 + ', ' + cor2 + ', ' + cor3 + ')';
 }
 
-
 function coresIniciais() {
-    const circulosColoridos = document.getElementsByClassName('ball');
     const corSelecionada = gerarCores();
     const circuloSelecionado = Math.floor(Math.random() * 6);
+    circuloCerto = circuloSelecionado
     const subtitulo = document.getElementById('rgb-color');
     subtitulo.innerText = corSelecionada;
     for (let i = 0; i < circulosColoridos.length; i += 1){
@@ -20,5 +23,29 @@ function coresIniciais() {
         }
     }
 }
-
 coresIniciais();
+
+function jogoEvento() {
+  for (let i = 0; i < circulosColoridos.length; i += 1) {
+    if (i !== circuloCerto) {
+      circulosColoridos[i].addEventListener('click', errado);
+    } else {
+      circulosColoridos[i].addEventListener('click', certo)
+    }
+  }
+}
+
+jogoEvento();
+
+function certo() {
+    FraseAcerto.innerText = 'Acertou!';
+    coresIniciais();
+    jogoEvento();
+}
+
+function errado() {
+    FraseAcerto.innerText = 'Errou! Tente novamente!';
+    coresIniciais();
+    jogoEvento();
+}
+
