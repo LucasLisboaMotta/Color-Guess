aa81562637051decedf8ca89b0cf729482ceb41a
Lucs Lisboa <lucas@pop-os.localdomain>
2021-11-25 14:44:50 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,61 +1,75 @@
-let circuloCerto
-let pontos = 0
+let circuloCerto;
+let pontos = 0;
 const circulosColoridos = document.getElementsByClassName('ball');
 const FraseAcerto = document.getElementById('answer');
-const placar = document.getElementById('score')
+const placar = document.getElementById('score');
 
 function gerarCores() {
-    const cor1 = Math.floor(Math.random() * 255);
-    const cor2 = Math.floor(Math.random() * 255);
-    const cor3 = Math.floor(Math.random() * 255);
-    return '(' + cor1 + ', ' + cor2 + ', ' + cor3 + ')';
+  console.log('gerarCores');
+  const cor1 = Math.floor(Math.random() * 255);
+  const cor2 = Math.floor(Math.random() * 255);
+  const cor3 = Math.floor(Math.random() * 255);
+  return '(' + cor1 + ', ' + cor2 + ', ' + cor3 + ')';
 }
 
 function coresIniciais() {
-    const corSelecionada = gerarCores();
-    const circuloSelecionado = Math.floor(Math.random() * 6);
-    circuloCerto = circuloSelecionado
-    const subtitulo = document.getElementById('rgb-color');
-    subtitulo.innerText = corSelecionada;
-    for (let i = 0; i < circulosColoridos.length; i += 1){
-        if (i === circuloSelecionado) {
-            circulosColoridos[i].style.backgroundColor = 'rgb' + corSelecionada;
-        } else {
-            circulosColoridos[i].style.backgroundColor = 'rgb' + gerarCores();
-        }
+  const corSelecionada = gerarCores();
+  const circuloSelecionado = Math.floor(Math.random() * 6);
+  circuloCerto = circuloSelecionado;
+  const subtitulo = document.getElementById('rgb-color');
+  subtitulo.innerText = corSelecionada;
+  for (let i = 0; i < circulosColoridos.length; i += 1) {
+    if (i === circuloSelecionado) {
+      circulosColoridos[i].style.backgroundColor = 'rgb' + corSelecionada;
+    } else {
+      circulosColoridos[i].style.backgroundColor = 'rgb' + gerarCores();
     }
+  }
 }
-coresIniciais();
 
 function jogoEvento() {
   for (let i = 0; i < circulosColoridos.length; i += 1) {
     if (i !== circuloCerto) {
       circulosColoridos[i].addEventListener('click', errado);
     } else {
-      circulosColoridos[i].addEventListener('click', certo)
+      circulosColoridos[i].addEventListener('click', certo);
+    }
+  }
+}
+window.addEventListener('load', coresIniciais);
+window.addEventListener('load', jogoEvento);
+
+function removerEvento() {
+  for (let i = 0; i < circulosColoridos.length; i += 1) {
+    if (i !== circuloCerto) {
+      circulosColoridos[i].removeEventListener('click', errado);
+    } else {
+      circulosColoridos[i].removeEventListener('click', certo);
     }
   }
 }
 
-jogoEvento();
-
 function certo() {
-    FraseAcerto.innerText = 'Acertou!';
-    pontos += 3;
-    placar.innerText = pontos;
-    coresIniciais();
-    jogoEvento();
+  removerEvento();
+  FraseAcerto.innerText = 'Acertou!';
+  pontos += 3;
+  placar.innerText = pontos;
+  coresIniciais();
+  jogoEvento();
 }
 
 function errado() {
-    FraseAcerto.innerText = 'Errou! Tente novamente!';
-    coresIniciais();
-    jogoEvento();
+  removerEvento();
+  FraseAcerto.innerText = 'Errou! Tente novamente!';
+  coresIniciais();
+  jogoEvento();
 }
 
 function reinicia() {
-    FraseAcerto.innerText = 'Escolha uma cor';
-    coresIniciais();
+  FraseAcerto.innerText = 'Escolha uma cor';
+  removerEvento();
+  coresIniciais();
+  jogoEvento();
 }
 
 const botaoReinicia = document.getElementById('reset-game');
